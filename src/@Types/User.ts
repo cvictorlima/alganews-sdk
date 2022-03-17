21f7169f49c3907c2455182e6eb9ed10da03f299
cvictorlima <89553541+cvictorlima@users.noreply.github.com>
2022-03-17 15:44:02 -0300

    update user service

--- src/@Types/User.ts
+++ src/@Types/User.ts
@@ -2,6 +2,7 @@ import { AlgaNews } from "./AlgaNews";
 
 export namespace User{
   
+  export type Summary = AlgaNews.components['schemas']['UserSummary']
   export type Metrics = AlgaNews.components['schemas']['UserMetrics']
   export type Input = AlgaNews.components['schemas']['UserInput']
   export type IdInput = AlgaNews.components['schemas']['UserIdInput']
