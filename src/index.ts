bcaad6c059c4acb860be1ea7997beedd8405a737
cvictorlima <89553541+cvictorlima@users.noreply.github.com>
2022-03-19 00:52:50 -0300

    patch

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as types from './@Types'
 
 export { default as FileService } from './services/File.service'
 export { default as MetricService } from './services/Metric.service'
-export { default as PostService } from './services/File.service'
+export { default as PostService } from './services/Post.service'
 export { default as UserService } from './services/User.service'
 export { default as PaymentService } from './services/Payment.service'
 export { default as CashFlowService } from './services/CashFlow.service'
